702b3898d36160ad01b4fd95f45b75e58874ed39
dominika-midor <60156527+dominika-midor@users.noreply.github.com>
2020-01-26 20:35:11 +0100

    Update Karnety.h

--- Karnety.h
+++ Karnety.h
@@ -5,27 +5,24 @@ using namespace std;
 #ifndef __Karnety_h__
 #define __Karnety_h__
 
-#include "Sta³y_klient.h"
+#include "StaÂ³y_klient.h"
 // #include "Administrator.h"
 
-class Sta³y_klient;
+class StaÂ³y_klient;
 class Administrator;
 class Karnety;
 
 class Karnety
-{
-	private: int _czas_zakupu;
-	private: int _okres_wa¿noœci;
-	private: bool _rabat;
-	private: string _kod;
-	public: Sta³y_klient* _unnamed_Sta³y_klient_;
-	public: Administrator* _unnamed_Administrator_;
-
-	public: void kup_karnet(Sta³y_klient aKlient, string aRabat);
-
-	public: void przed³u¿_karnet(Sta³y_klient aKlient, string aRabat);
-
-	public: void sprawdz_kod_rabatowy(string aKod);
+{private: 
+ 	int czas_zakupu;
+	int okres_waÂ¿noÅ“ci;
+	bool rabat;
+	string kod;
+public: 
+ 
+    void kup_karnet(Staly_klient klient, bool rabat);
+    void przedluz_karnet(Staly_klient klient, bool rabat);
+    void sprawdz_kod_rabatowy(string kod);
 };
 
 #endif
